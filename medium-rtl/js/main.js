620aae075cdaf08cbcdc08507f1a88128658712c
zahranisa <samy@jehazy.com>
2016-09-05 13:31:54 +0300

    remove jQuery use mutation events in place of transition end events

--- medium-rtl/js/main.js
+++ medium-rtl/js/main.js
@@ -1,19 +1,24 @@
-
-
-function main () {
-
-  var content = $('.postArticle-content')
-
-  if (content.text().isArabic()) {
-    content.attr('dir', 'rtl')
-  }
-}
-
-main()
-
-document.body.addEventListener("transitionend", function (event) {
-  
-  if (/js-heroPromo/.test(event.target.className)) {
-    main()
-  }
-}, false)
+// IIFE
+(function main () {
+  const textContainerQuery = '.postArticle-content';
+  let content = document.querySelector('#container')
+  let observer = new MutationObserver((mutations) => {
+    let addedNodes = [].reduce.call(mutations, (prev, cur) => {
+      if (cur.addedNodes.length) {
+        // cast NodeLists to Arrays
+        let rtlNodes = [...cur.addedNodes]
+          // get rtlable elements
+          .map(node => [...node.querySelectorAll(textContainerQuery)])
+          .filter(list => list.length);
+        return prev.concat.apply(prev, rtlNodes);
+      }
+      return prev;
+    }, []);
+    addedNodes.forEach(node => {
+      if (node.textContent && node.textContent.isArabic()) {
+        node.setAttribute('dir', 'rtl');
+      }
+    })
+  })
+  observer.observe(content, { childList: true }); 
+})()
